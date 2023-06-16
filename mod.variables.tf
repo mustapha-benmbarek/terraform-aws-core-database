9e427db008315d0ca96b2b00c00cd0800f3903fe
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-16 18:41:09 +0400

    Initial commit

--- mod.variables.tf
+++ mod.variables.tf
@@ -8,9 +8,17 @@ variable "file-rds-postgresql-dbs" { default = "" }
 variable "file-rds-mariadb-dbs" { default = "" }
 variable "file-rds-oracle-dbs" { default = "" }
 variable "file-rds-mssql-dbs" { default = "" }
-variable "file-file-documentdb-dbs" { default = "" }
-variable "file-file-neptune-dbs" { default = "" }
-variable "file-file-elasticache-dbs" { default = "" }
-variable "file-file-memorydb-redis-dbs" { default = "" }
-variable "file-file-quantum-ledger-dbs" { default = "" }
-variable "file-file-keyspaces-dbs" { default = "" }
+
+variable "file-documentdb-dbs" { default = "" }
+variable "file-documentdb-subnet-groups" { default = "" }
+variable "file-documentdb-parameter-groups" { default = "" }
+
+
+variable "file-neptune-dbs" { default = "" }
+variable "file-neptune-subnet-groups" { default = "" }
+variable "file-neptune-parameter-groups" { default = "" }
+
+variable "file-elasticache-dbs" { default = "" }
+variable "file-memorydb-redis-dbs" { default = "" }
+variable "file-quantum-ledger-dbs" { default = "" }
+variable "file-keyspaces-dbs" { default = "" }
