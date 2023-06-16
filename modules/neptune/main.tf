4ab3532c74d94ea2573f7117ccb8080b28e2d7f9
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-16 19:05:33 +0400

    Initial commit

--- modules/neptune/main.tf
+++ modules/neptune/main.tf
@@ -11,7 +11,7 @@ resource "aws_neptune_subnet_group" "this_subnet_group" {
 
 /*Resource creation: Neptune Parameter Group*/
 resource "aws_neptune_parameter_group" "this_parameter_group" {
-  for_each    = { for k, user in var.core-db-parameter-groups : k => pg if pg.cluster }
+  for_each    = { for k, pg in var.core-db-parameter-groups : k => pg if pg.cluster }
   name        = each.value.name
   description = each.value.description
   family      = each.value.family
@@ -31,7 +31,7 @@ resource "aws_neptune_parameter_group" "this_parameter_group" {
 /*Resource creation: Neptune Cluster Parameter Group*/
 resource "aws_neptune_cluster_parameter_group" "this_cluster_parameter_group" {
   //for_each    = var.core-db-parameter-groups
-  for_each    = { for k, user in var.core-db-parameter-groups : k => pg if pg.cluster }
+  for_each    = { for k, pg in var.core-db-parameter-groups : k => pg if pg.cluster }
   name        = each.value.name
   description = each.value.description
   family      = each.value.family
